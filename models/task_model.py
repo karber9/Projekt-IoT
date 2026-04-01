43bf8e42a1436385fab1b69120444be9899fdb88
aniaszkaradek <ania.szkaradek@interia.pl>
2026-04-01 09:36:42 +0200

    task_model.py

--- models/task_model.py
+++ models/task_model.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import DeclarativeBase
 
+#Base class for all SQLAlchemy models
 class Base(DeclarativeBase):
     pass
 
+#Database table model for tasks
 class Task(Base):
     __tablename__ = "tasks"
 
