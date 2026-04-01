1d3a41742b41716b15910c43ac70b1d6c6f3981b
aniaszkaradek <ania.szkaradek@interia.pl>
2026-04-01 09:39:51 +0200

    task_schema.py

--- schemas/task_schema.py
+++ schemas/task_schema.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 
+#Schema for incoming request - data sent by user
 class TaskCreate(BaseModel):
     payload: str
 
+#Schema for API responses - data returned to user
 class TaskResponse(BaseModel):
     id: int
     status: str
